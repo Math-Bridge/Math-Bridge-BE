109b9d14adcdef845886ff7220da42dea94c8674
Phạm Anh Kiệt <kietpase183731@fpt.edu.vn>
2025-09-07 23:33:05 +0700

    Update database, update methods for wallet, update config

--- MathBridgeSystem.Infrastructure/Repositories/WalletTransactionRepository.cs
+++ MathBridgeSystem.Infrastructure/Repositories/WalletTransactionRepository.cs
@@ -18,7 +18,35 @@ namespace MathBridge.Infrastructure.Repositories
         {
             return await _context.WalletTransactions
                 .Where(wt => wt.ParentId == parentId)
+                .OrderByDescending(wt => wt.TransactionDate)
                 .ToListAsync();
+        }
+
+        public async Task<WalletTransaction> AddAsync(WalletTransaction transaction)
+        {
+            _context.WalletTransactions.Add(transaction);
+            await _context.SaveChangesAsync();
+            return transaction;
+        }
+
+        public async Task<WalletTransaction> UpdateAsync(WalletTransaction transaction)
+        {
+            _context.WalletTransactions.Update(transaction);
+            await _context.SaveChangesAsync();
+            return transaction;
+        }
+
+        public async Task<WalletTransaction?> GetByIdAsync(Guid transactionId)
+        {
+            return await _context.WalletTransactions
+                .Include(wt => wt.Parent)
+                .FirstOrDefaultAsync(wt => wt.TransactionId == transactionId);
+        }
+
+        public async Task<bool> ExistsAsync(Guid transactionId)
+        {
+            return await _context.WalletTransactions
+                .AnyAsync(wt => wt.TransactionId == transactionId);
         }
     }
 }
