86e5e651f6d6f617eb8a982a755e02081066b67d
huy69185 <huyh69185@gmail.com>
2025-09-22 23:28:15 +0700

    ChildDto Done

---
+++ MathBridgeSystem.Application/DTOs/ChildDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathBridge.Application.DTOs
+{
+    public class ChildDto
+    {
+        public Guid ChildId { get; set; }
+        public string FullName { get; set; }
+        public Guid SchoolId { get; set; }
+        public string SchoolName { get; set; }
+        public Guid? CenterId { get; set; }
+        public string? CenterName { get; set; }
+        public string Grade { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string Status { get; set; }
+    }
+}
