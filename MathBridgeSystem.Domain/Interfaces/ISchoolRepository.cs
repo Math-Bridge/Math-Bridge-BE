a9ecb5be5c4c8f91d02ce30a589aa3b1c1188b82
huy69185 <huyh69185@gmail.com>
2025-09-22 23:42:37 +0700

    updated ISchoolRepository

--- MathBridgeSystem.Domain/Interfaces/ISchoolRepository.cs
+++ MathBridgeSystem.Domain/Interfaces/ISchoolRepository.cs
@@ -3,14 +3,14 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace MathBridgeSystem.Domain.Interfaces
+namespace MathBridge.Domain.Interfaces
 {
     public interface ISchoolRepository
     {
         Task AddAsync(School school);
+        Task UpdateAsync(School school);
         Task<School?> GetByIdAsync(Guid id);
         Task<IEnumerable<School>> GetAllAsync();
-        Task UpdateAsync(School school);
         Task DeleteAsync(Guid id);
         Task<bool> ExistsAsync(Guid id);
     }
