fce25acb951ff58cf34338b7d93ee376288f1449
huy69185 <huyh69185@gmail.com>
2025-09-22 23:30:17 +0700

    CreatePackageRequest Done

---
+++ MathBridgeSystem.Application/DTOs/CreatePackageRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathBridge.Application.DTOs
+{
+    public class CreatePackageRequest
+    {
+        public string PackageName { get; set; }
+        public Guid ProgramId { get; set; }
+        public string Grade { get; set; }
+        public decimal Price { get; set; }
+        public int SessionCount { get; set; }
+        public int SessionsPerWeek { get; set; }
+        public int MaxReschedule { get; set; }
+        public int DurationDays { get; set; }
+        public string? Description { get; set; }
+    }
+}
