c66894fc118aaa89a42234f406ecdf117c13b106
huy69185 <huyh69185@gmail.com>
2025-09-22 23:32:55 +0700

    UpdateCenterRequest Done

---
+++ MathBridgeSystem.Application/DTOs/UpdateCenterRequest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace MathBridge.Application.DTOs
+{
+    public class UpdateCenterRequest
+    {
+        public string? Name { get; set; }
+        public string? PlaceId { get; set; }
+    }
+}
