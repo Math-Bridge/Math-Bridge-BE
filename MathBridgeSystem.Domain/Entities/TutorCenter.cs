44258da9173bbff727aa86316373bbf526ace7bb
huy69185 <huyh69185@gmail.com>
2025-09-22 23:24:08 +0700

    Done TutorVerification

--- MathBridgeSystem.Domain/Entities/TutorCenter.cs
+++ MathBridgeSystem.Domain/Entities/TutorCenter.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MathBridgeSystem.Domain.Entities
+namespace MathBridge.Domain.Entities
 {
-    internal class TutorCenter
+    public partial class TutorCenter
     {
+        public Guid TutorCenterId { get; set; }
+        public Guid TutorId { get; set; }
+        public Guid CenterId { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+        [ForeignKey("TutorId")]
+        public virtual User Tutor { get; set; } = null!;
+
+        [ForeignKey("CenterId")]
+        public virtual Center Center { get; set; } = null!;
     }
 }
