7ac3f4d116ac7298ffe8cf9b74bb9535be986354
huy69185 <huyh69185@gmail.com>
2025-09-22 23:43:02 +0700

    ITutorCenterRepository Done

---
+++ MathBridgeSystem.Domain/Interfaces/ITutorCenterRepository.cs
@@ -0,0 +1,18 @@
+﻿using MathBridge.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathBridgeSystem.Domain.Interfaces
+{
+    public interface ITutorCenterRepository
+    {
+        Task AddAsync(TutorCenter tutorCenter);
+        Task RemoveAsync(Guid tutorCenterId);
+        Task<List<TutorCenter>> GetByTutorIdAsync(Guid tutorId);
+        Task<List<TutorCenter>> GetByCenterIdAsync(Guid centerId);
+        Task<bool> TutorIsAssignedToCenterAsync(Guid tutorId, Guid centerId);
+    }
+}
