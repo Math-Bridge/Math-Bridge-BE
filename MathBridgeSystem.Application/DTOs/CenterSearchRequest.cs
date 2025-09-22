d132703d2fedc3bbecada4793c5a22355cdbc6ce
huy69185 <huyh69185@gmail.com>
2025-09-22 23:27:08 +0700

    CenterSearchRequest Done

---
+++ MathBridgeSystem.Application/DTOs/CenterSearchRequest.cs
@@ -0,0 +1,15 @@
+﻿
+namespace MathBridge.Application.DTOs
+{
+    public class CenterSearchRequest
+    {
+        public string? Name { get; set; }
+        public string? City { get; set; }
+        public string? District { get; set; }
+        public double? Latitude { get; set; }
+        public double? Longitude { get; set; }
+        public double? RadiusKm { get; set; } = 10.0;
+        public int Page { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+    }
+}
