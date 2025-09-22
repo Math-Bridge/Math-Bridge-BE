97953c581ed033f9bc916264758687de1ed91c69
huy69185 <huyh69185@gmail.com>
2025-09-22 23:41:33 +0700

    IMathProgramRepository Done

---
+++ MathBridgeSystem.Domain/Interfaces/IMathProgramRepository.cs
@@ -0,0 +1,15 @@
+﻿using MathBridge.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathBridge.Domain.Interfaces
+{
+    public interface IMathProgramRepository
+    {
+        Task<MathProgram> GetByIdAsync(Guid id);
+        Task<bool> ExistsAsync(Guid id);
+    }
+}
