c674c96fd84edb145091f0c0fcc81cb4d736fb86
huy69185 <huyh69185@gmail.com>
2025-09-22 23:29:51 +0700

    CreateContractRequest Done

---
+++ MathBridgeSystem.Application/DTOs/CreateContractRequest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathBridge.Application.DTOs
+{
+    public class CreateContractRequest
+    {
+        public Guid ParentId { get; set; }
+        public Guid ChildId { get; set; }
+        public Guid PackageId { get; set; }
+        public Guid MainTutorId { get; set; }
+        public Guid? SubstituteTutor1Id { get; set; }
+        public Guid? SubstituteTutor2Id { get; set; }
+        public Guid? CenterId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string TimeSlot { get; set; }
+        public bool IsOnline { get; set; }
+        public string? OfflineAddress { get; set; }
+        public decimal? OfflineLatitude { get; set; }
+        public decimal? OfflineLongitude { get; set; }
+        public string? VideoCallPlatform { get; set; }
+        public decimal MaxDistanceKm { get; set; }
+    }
+}
