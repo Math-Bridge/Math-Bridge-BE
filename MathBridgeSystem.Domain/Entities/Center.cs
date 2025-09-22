d6a04e69e32f5cae0ad54bd68b89bb4b85a824da
huy69185 <huyh69185@gmail.com>
2025-09-22 23:19:06 +0700

    Done Center class

--- MathBridgeSystem.Domain/Entities/Center.cs
+++ MathBridgeSystem.Domain/Entities/Center.cs
@@ -1,12 +1,32 @@
-﻿using System;
+﻿using MathBridge.Domain.Entities;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MathBridgeSystem.Domain.Entities
+namespace MathBridge.Domain.Entities
 {
-    internal class Center
+    public partial class Center
     {
+        public Guid CenterId { get; set; }
+        public string Name { get; set; } = null!;
+        public double? Latitude { get; set; }
+        public double? Longitude { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public string? GooglePlaceId { get; set; }
+        public string? FormattedAddress { get; set; }
+        public string? City { get; set; }
+        public string? District { get; set; }
+        public string? PlaceName { get; set; }
+        public string? CountryCode { get; set; }
+        public DateTime? LocationUpdatedDate { get; set; }
+        public int TutorCount { get; set; } = 0;
+
+        public virtual ICollection<Child> Children { get; set; } = new List<Child>();
+        public virtual ICollection<Contract> Contracts { get; set; } = new List<Contract>();
+        public virtual ICollection<TutorCenter> TutorCenters { get; set; } = new List<TutorCenter>();
     }
 }
