ff36366fc120b1fd061a0ea40556361bb0f98660
huy69185 <huyh69185@gmail.com>
2025-09-22 23:41:05 +0700

    IContractRepository Done

---
+++ MathBridgeSystem.Domain/Interfaces/IContractRepository.cs
@@ -0,0 +1,19 @@
+﻿using MathBridge.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathBridge.Domain.Interfaces
+{
+    public interface IContractRepository
+    {
+        Task AddAsync(Contract contract);
+        Task UpdateAsync(Contract contract);
+        Task<List<Contract>> GetByParentIdAsync(Guid parentId);
+        Task<List<Contract>> GetByChildIdAsync(Guid childId);
+        Task<Contract?> GetByIdAsync(Guid id);
+        Task<List<Contract>> GetByCenterIdAsync(Guid centerId);
+    }
+}
