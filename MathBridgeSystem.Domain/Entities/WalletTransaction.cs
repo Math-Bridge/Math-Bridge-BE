24c52f941ad7619b460850e12de9eae1450ce910
huy69185 <huyh69185@gmail.com>
2025-09-22 23:24:54 +0700

    WalletTransaction Updated

--- MathBridgeSystem.Domain/Entities/WalletTransaction.cs
+++ MathBridgeSystem.Domain/Entities/WalletTransaction.cs
@@ -6,26 +6,18 @@ namespace MathBridge.Domain.Entities;
 public partial class WalletTransaction
 {
     public Guid TransactionId { get; set; }
-
     public Guid ParentId { get; set; }
-
     public Guid? ContractId { get; set; }
-
     public decimal Amount { get; set; }
-
     public string TransactionType { get; set; } = null!;
-
     public string? Description { get; set; }
-
     public DateTime TransactionDate { get; set; }
-
     public string Status { get; set; } = null!;
-
     public string? PaymentMethod { get; set; }
-
     public string? PaymentGatewayReference { get; set; }
 
+    // Navigation Properties
     public virtual User Parent { get; set; } = null!;
-
+    public virtual Contract? Contract { get; set; } // Optional relationship
     public virtual ICollection<SePayTransaction> SePayTransactions { get; set; } = new List<SePayTransaction>();
 }
