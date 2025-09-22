9796f7d6a0d4c8893ed3871aa9b74898503cd968
huy69185 <huyh69185@gmail.com>
2025-09-22 23:32:07 +0700

    PaymentPackageDto Done

---
+++ MathBridgeSystem.Application/DTOs/PaymentPackageDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathBridge.Application.DTOs
+{
+    public class PaymentPackageDto
+    {
+        public Guid PackageId { get; set; }
+        public string PackageName { get; set; }
+        public Guid ProgramId { get; set; }
+        public string ProgramName { get; set; }
+        public string Grade { get; set; }
+        public decimal Price { get; set; }
+        public int SessionCount { get; set; }
+        public int SessionsPerWeek { get; set; }
+        public int MaxReschedule { get; set; }
+        public int DurationDays { get; set; }
+        public string? Description { get; set; }
+    }
+}
