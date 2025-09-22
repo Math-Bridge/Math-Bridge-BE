e05fb124c6223a732307ec801855b57a7c914dce
huy69185 <huyh69185@gmail.com>
2025-09-22 23:33:26 +0700

    UpdateChildRequest Done

---
+++ MathBridgeSystem.Application/DTOs/UpdateChildRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathBridge.Application.DTOs
+{
+    public class UpdateChildRequest
+    {
+        public string FullName { get; set; }
+        public Guid SchoolId { get; set; }
+        public Guid? CenterId { get; set; }
+        public string Grade { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+    }
+}
