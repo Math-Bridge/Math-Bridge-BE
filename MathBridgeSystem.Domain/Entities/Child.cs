bc76db472b9d3c53f29a43fe191f7d4d042f6dd0
huy69185 <huyh69185@gmail.com>
2025-09-22 23:21:22 +0700

    Child done

---
+++ MathBridgeSystem.Domain/Entities/Child.cs
@@ -0,0 +1,28 @@
+﻿using MathBridge.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathBridge.Domain.Entities
+{
+    public partial class Child
+    {
+        public Guid ChildId { get; set; }
+        public Guid ParentId { get; set; }
+        public string FullName { get; set; } = null!;
+        public Guid SchoolId { get; set; }
+        public Guid? CenterId { get; set; }
+        public string Grade { get; set; } = null!;
+        public DateTime? DateOfBirth { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string Status { get; set; } = "active"; // Added for soft delete: 'active' or 'deleted'
+
+        public virtual User Parent { get; set; } = null!;
+        public virtual School School { get; set; } = null!;
+        public virtual Center? Center { get; set; }
+        public virtual ICollection<Contract> Contracts { get; set; } = new List<Contract>();
+    }
+}
