cb1d8f1af90d47e1d62b62ded6e6dc46eb17b0ca
huy69185 <huyh69185@gmail.com>
2025-09-22 23:31:42 +0700

    MathProgramDto Done

---
+++ MathBridgeSystem.Application/DTOs/MathProgramDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathBridge.Application.DTOs
+{
+    public class MathProgramDto
+    {
+        public Guid ProgramId { get; set; }
+        public string ProgramName { get; set; }
+        public string? Description { get; set; }
+    }
+}
