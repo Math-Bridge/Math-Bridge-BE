6481662064e4feec7a45e40466a68ac6b8ecc604
huy69185 <huyh69185@gmail.com>
2025-09-22 23:40:10 +0700

    ICenterRepository interface done

---
+++ MathBridgeSystem.Domain/Interfaces/ICenterRepository.cs
@@ -0,0 +1,23 @@
+﻿using MathBridge.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MathBridge.Domain.Interfaces
+{
+    public interface ICenterRepository
+    {
+        Task AddAsync(Center center);
+        Task UpdateAsync(Center center);
+        Task DeleteAsync(Guid id); // Added
+        Task<Center?> GetByIdAsync(Guid id);
+        Task<List<Center>> GetAllAsync();
+        Task<List<Center>> GetByCityAsync(string city);
+        Task<List<Center>> GetByCoordinates(double latitude, double longitude, double radiusKm);
+        Task<bool> ExistsAsync(Guid id);
+        Task UpdateTutorCountAsync(Guid centerId, int increment);
+    }
+}
