ba077787c05ab0234f23f9e4e4a7117d5c34f321
huy69185 <huyh69185@gmail.com>
2025-09-22 23:30:53 +0700

    GeocodeResponse Done

---
+++ MathBridgeSystem.Application/DTOs/GeocodeResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathBridge.Application.DTOs
+{
+    public class GeocodeResponse
+    {
+        public bool Success { get; set; }
+        public double? Latitude { get; set; }
+        public double? Longitude { get; set; }
+        public string? FormattedAddress { get; set; }
+        public string? City { get; set; }
+        public string? District { get; set; }
+        public string? ErrorMessage { get; set; }
+
+        public bool HasValidCoordinates => Success && Latitude.HasValue && Longitude.HasValue;
+    }
+}
