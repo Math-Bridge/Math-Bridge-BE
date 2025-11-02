2401a08481e258e718f17c5825d3be9891990147
huy69185 <huyh69185@gmail.com>
2025-11-02 15:37:24 +0700

    Create CurriculumSearchRequest

---
+++ MathBridgeSystem.Application/DTOs/Curriculum/CurriculumSearchRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MathBridgeSystem.Application.DTOs.Curriculum
+{
+    public class CurriculumSearchRequest
+    {
+        public string? Name { get; set; }
+
+        public string? Code { get; set; }
+
+        public string? Grades { get; set; }
+
+        public bool? IsActive { get; set; }
+
+        [Range(1, int.MaxValue)]
+        public int Page { get; set; } = 1;
+
+        [Range(1, 100)]
+        public int PageSize { get; set; } = 20;
+    }
+}
