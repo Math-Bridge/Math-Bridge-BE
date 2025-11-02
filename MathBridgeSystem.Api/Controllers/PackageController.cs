cd2c16e493a21a5b255b38713d58733c31104a50
huy69185 <huyh69185@gmail.com>
2025-11-02 17:06:23 +0700

    Done Payment Package

---
+++ MathBridgeSystem.Api/Controllers/PackageController.cs
@@ -0,0 +1,28 @@
+﻿using MathBridgeSystem.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+[Route("api/packages")]
+[ApiController]
+public class PackageController : ControllerBase
+{
+    private readonly IPackageService _packageService;
+
+    public PackageController(IPackageService packageService)
+    {
+        _packageService = packageService ?? throw new ArgumentNullException(nameof(packageService));
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllPackages()
+    {
+        try
+        {
+            var packages = await _packageService.GetAllPackagesAsync();
+            return Ok(packages);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, new { error = ex.Message });
+        }
+    }
+}
