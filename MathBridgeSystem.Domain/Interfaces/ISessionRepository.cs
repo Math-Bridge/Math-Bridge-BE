b9de9352ede166bd0a262d5ff8fd64508b18617d
huy69185 <huyh69185@gmail.com>
2025-11-02 18:30:36 +0700

    Done ISessionRepository.cs

--- MathBridgeSystem.Domain/Interfaces/ISessionRepository.cs
+++ MathBridgeSystem.Domain/Interfaces/ISessionRepository.cs
@@ -1,12 +1,15 @@
-﻿using System;
+﻿using MathBridgeSystem.Domain.Entities;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace MathBridgeSystem.Domain.Interfaces
 {
-    internal class ISessionRepository
+    public interface ISessionRepository
     {
+        Task AddRangeAsync(IEnumerable<Session> sessions);
+        Task<Session?> GetByIdAsync(Guid bookingId);
+        Task UpdateAsync(Session session);
+        Task<bool> IsTutorAvailableAsync(Guid tutorId, DateOnly date, TimeSpan startTime, TimeSpan endTime);
     }
 }
