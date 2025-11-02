df379144f10a5c6cfe32ca1ef8423052d81714c8
huy69185 <huyh69185@gmail.com>
2025-11-02 18:37:06 +0700

    Done IRescheduleService.cs

--- MathBridgeSystem.Application/Interfaces/IRescheduleService.cs
+++ MathBridgeSystem.Application/Interfaces/IRescheduleService.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using MathBridgeSystem.Application.DTOs;
+using System;
 using System.Threading.Tasks;
 
 namespace MathBridgeSystem.Application.Interfaces
 {
-    internal class IRescheduleService
+    public interface IRescheduleService
     {
+        Task<RescheduleResponseDto> CreateRequestAsync(Guid parentId, CreateRescheduleRequestDto dto);
+        Task<RescheduleResponseDto> ApproveRequestAsync(Guid staffId, Guid requestId, ApproveRescheduleRequestDto dto);
+        Task<RescheduleResponseDto> RejectRequestAsync(Guid staffId, Guid requestId, string reason);
     }
 }
