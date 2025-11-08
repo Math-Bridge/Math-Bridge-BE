b7d41f14ecf8d5bc61f384a4e9afaec2833c2f12
huy69185 <huyh69185@gmail.com>
2025-11-08 16:59:23 +0700

    Update SessionStatusRequest

--- MathBridgeSystem.Test/obj/Debug/net8.0/MathBridgeSystem.Test.AssemblyInfo.cs
+++ MathBridgeSystem.Test/obj/Debug/net8.0/MathBridgeSystem.Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MathBridgeSystem.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78af30410f442994ec3eb034837c67a9583072f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa8d883acbf9a25e6786d30266f1e722602349fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("MathBridgeSystem.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MathBridgeSystem.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
