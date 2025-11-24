a090b5f4d33d513714673bcb7e94d20df349c869
Phạm Anh Kiệt <kietpase183731@fpt.edu.vn>
2025-11-24 16:06:14 +0700

    allow banned

--- MathBridgeSystem.Application/DTOs/UpdateStatusRequest.cs
+++ MathBridgeSystem.Application/DTOs/UpdateStatusRequest.cs
@@ -5,7 +5,7 @@ namespace MathBridgeSystem.Application.DTOs
     public class UpdateStatusRequest
     {
         [Required(ErrorMessage = "Status is required")]
-        [RegularExpression("^(active|inactive|deleted)$", ErrorMessage = "Status not found. Allowed values are: active, inactive, deleted")]
+        [RegularExpression("^(active|inactive|deleted|banned)$", ErrorMessage = "Status not found. Allowed values are: active, inactive, deleted")]
         public string Status { get; set; }
     }
 }
