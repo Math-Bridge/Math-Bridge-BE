aca0c8e999420bc0cb00ade8fbf4887de82c8644
huy69185 <huyh69185@gmail.com>
2025-11-29 18:33:32 +0700

    Create ReAssignTutorRequest.cs

---
+++ MathBridgeSystem.Application/ReAssignTutorRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MathBridgeSystem.Application.DTOs
+{
+    public class ReAssignTutorRequest
+    {
+        [Required] public Guid BannedTutorId { get; set; }        
+        [Required] public Guid ReplacementTutorId { get; set; }     
+        public bool ApplyToAllUpcomingSessions { get; set; } = true; 
+    }
+}
