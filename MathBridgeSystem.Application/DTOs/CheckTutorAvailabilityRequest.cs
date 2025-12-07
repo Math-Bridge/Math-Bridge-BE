685d851cce4b8f2bd09def796934119fb764e5c0
huy69185 <huyh69185@gmail.com>
2025-12-07 23:43:24 +0700

    Create new DTo for check available before create

---
+++ MathBridgeSystem.Application/DTOs/CheckTutorAvailabilityRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathBridgeSystem.Application.DTOs
+{
+    public class CheckTutorAvailabilityRequest
+    {
+        [Required] public Guid PackageId { get; set; }
+        [Required] public Guid ChildId { get; set; }
+        [Required] public DateOnly StartDate { get; set; }
+        [Required] public DateOnly EndDate { get; set; }
+        [Required] public List<ContractScheduleDto> Schedules { get; set; } = new();
+        [Required] public bool IsOnline { get; set; }
+        public string? OfflineAddress { get; set; }
+        public decimal? OfflineLatitude { get; set; }
+        public decimal? OfflineLongitude { get; set; }
+        public decimal? MaxDistanceKm { get; set; } = 15;
+    }
+}
